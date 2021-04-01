ceb46406343914d9b48ddd1d4444009d69f29060
Roch Wojtowicz <roch.wojtowicz@student.uj.edu.pl>
2021-04-01 18:00:37 +0200

    Template project file added

---
+++ labyrinths/src/main/java/labyrinths/SecondaryController.java
@@ -0,0 +1,12 @@
+package labyrinths;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
