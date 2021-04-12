9d4890b9531197ade1b936b859273333d7cf8b05
Roch Wojtowicz <roch.wojtowicz@student.uj.edu.pl>
2021-04-13 00:26:39 +0200

    Default labyrinth added

--- src/main/java/labyrinths/controller/LabyrinthController.java
+++ src/main/java/labyrinths/controller/LabyrinthController.java
@@ -9,8 +9,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import labyrinths.App;
+import labyrinths.model.Field;
+import labyrinths.model.Labyrinth;
+import labyrinths.model.Result;
 
 import java.io.IOException;
 import java.net.URL;
@@ -25,17 +29,21 @@ public class LabyrinthController implements Initializable {
     Pane labyrinthPane;
 
     List<List<Button>> fields;
+    Labyrinth labyrinthModel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //labyrinthPane.setBackground(new Background(new BackgroundFill(Color.ROSYBROWN, CornerRadii.EMPTY, Insets.EMPTY)));
-        constructEmptyLabyrinth(15, 30);
+        int width = 15, height = 30;
+        constructEmptyLabyrinth(width, height);
+        Labyrinth labyrinthModel = new Labyrinth(width, height);
+        //applyChanges(labyrinthModel.getDefault());
     }
-    void constructEmptyLabyrinth(int wight, int height) {
+    void constructEmptyLabyrinth(int width, int height) {
         fields = new ArrayList<>();
         for(int i=0; i<height; ++i) {
             List<Button> row = new ArrayList<>();
-            for (int j = 0; j < wight; ++j) {
+            for (int j = 0; j < width; ++j) {
                 Button field = new Button("");
                 field.prefWidthProperty().bind(labyrinthPane.widthProperty());
                 field.prefHeightProperty().bind(labyrinthPane.heightProperty());
@@ -46,12 +54,30 @@ public class LabyrinthController implements Initializable {
 
         GridPane labyrinth = new GridPane();
         for(int i=0; i<height; ++i) {
-            for(int j=0; j<wight; ++j){
+            for(int j=0; j<width; ++j){
                 labyrinth.add(fields.get(i).get(j), i, j);
             }
         }
         labyrinth.prefWidthProperty().bind(labyrinthPane.widthProperty());
         labyrinth.prefHeightProperty().bind(labyrinthPane.heightProperty());
         labyrinthPane.getChildren().add(labyrinth);
+    }
+
+    void applyChanges(Result result) {
+        for(Field field : result.getChanges()) {
+            Button button = fields.get(field.getX()).get(field.getY());
+            switch (field.getType()) {
+                case WALL:
+                    button.setBackground(new Background(
+                            new BackgroundFill(Color.DEEPSKYBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+                    break;
+                case START:
+                    button.setText("S");
+                    break;
+                case TARGET:
+                    button.setText("T");
+                    break;
+            }
+        }
     }
 }
