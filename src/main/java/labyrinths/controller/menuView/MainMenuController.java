76b3082204f3ffc5683a6c5f812cbfaaa6bc0f96
Kuba <jakub8.kowalczyk@student.uj.edu.pl>
2021-05-24 20:33:16 +0200

    Added fileChooser and main menu works with new LabyrinthGetter

--- src/main/java/labyrinths/controller/menuView/MainMenuController.java
+++ src/main/java/labyrinths/controller/menuView/MainMenuController.java
@@ -3,11 +3,14 @@ package labyrinths.controller.menuView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import labyrinths.App;
 import labyrinths.controller.labyrinthView.LabyrinthGetter;
+import labyrinths.model.Labyrinth;
 import labyrinths.model.LabyrinthPreset;
 
 import java.io.File;
@@ -19,25 +22,44 @@ public class MainMenuController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        configureFileChooser();
         initializeControls();
     }
+    File labyrinthFile;
+    FileChooser fileChooser;
 
     @FXML
-    Button preset10x10Btn, preset20x40Btn, preset15x30Btn, confirmBtn;
+    Button preset10x10Btn, preset20x40Btn, preset15x30Btn, confirmBtn, chooseFileBtn;
     @FXML
     Slider heightSlider, widthSlider;
     @FXML
     ImageView preset10x10Img, preset15x30Img, preset20x40Img;
+    @FXML
+    Label fileLabel;
 
     Image preset10x10 = new Image(new File("src/main/resources/drawable/preset_10x10.png").toURI().toString());
     Image preset15x30 = new Image(new File("src/main/resources/drawable/preset_15x30.png").toURI().toString());
     Image preset20x40 = new Image(new File("src/main/resources/drawable/preset_20x40.png").toURI().toString());
 
+    void configureFileChooser() {
+        fileChooser = new FileChooser();
+        fileChooser.setTitle("Select labyrinth file");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("LAB file", "*.lab"));
+    }
+
     void initializeControls() {
+
+        chooseFileBtn.setOnAction(actionEvent -> {
+            labyrinthFile = fileChooser.showOpenDialog(App.mainStage);
+            if(labyrinthFile != null) {
+                fileLabel.setText(labyrinthFile.getName());
+            }
+        });
 
         preset10x10Btn.setOnAction(actionEvent -> {
             try {
-                App.mainStage.setScene(LabyrinthGetter.getLabyrinthScene(10,10,LabyrinthPreset.PRESET_10x10));
+                Labyrinth labyrinth = new Labyrinth(10,10);
+                App.mainStage.setScene(LabyrinthGetter.getLabyrinthScene(labyrinth, labyrinth.getPreset(LabyrinthPreset.PRESET_10x10)));
                 App.mainStage.setMaximized(true);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -47,7 +69,8 @@ public class MainMenuController implements Initializable {
 
         preset15x30Btn.setOnAction(actionEvent -> {
             try {
-                App.mainStage.setScene(LabyrinthGetter.getLabyrinthScene(15,30,LabyrinthPreset.PRESET_15x30));
+                Labyrinth labyrinth = new Labyrinth(15,30);
+                App.mainStage.setScene(LabyrinthGetter.getLabyrinthScene(labyrinth, labyrinth.getPreset(LabyrinthPreset.PRESET_15x30)));
                 App.mainStage.setMaximized(true);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -57,7 +80,8 @@ public class MainMenuController implements Initializable {
 
         preset20x40Btn.setOnAction(actionEvent -> {
             try {
-                App.mainStage.setScene(LabyrinthGetter.getLabyrinthScene(20,40,LabyrinthPreset.PRESET_20x40));
+                Labyrinth labyrinth = new Labyrinth(20,40);
+                App.mainStage.setScene(LabyrinthGetter.getLabyrinthScene(labyrinth, labyrinth.getPreset(LabyrinthPreset.PRESET_20x40)));
                 App.mainStage.setMaximized(true);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -67,8 +91,9 @@ public class MainMenuController implements Initializable {
 
         confirmBtn.setOnAction((actionEvent -> {
             try {
+                Labyrinth labyrinth = new Labyrinth((int) heightSlider.getValue(),(int) widthSlider.getValue());
                 App.mainStage.setScene(LabyrinthGetter.
-                        getLabyrinthScene((int) heightSlider.getValue(),(int) widthSlider.getValue(), LabyrinthPreset.DEFAULT));
+                        getLabyrinthScene(labyrinth, labyrinth.getPreset(LabyrinthPreset.DEFAULT)));
                 App.mainStage.setMaximized(true);
             } catch (IOException e) {
                 e.printStackTrace();
